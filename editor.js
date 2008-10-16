2334033b218d576bbd67f5557672ad1d9fbf2655
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2008-10-16 15:49:03 -0700

    extracted editor to own js, some more changes

---
+++ editor.js
@@ -0,0 +1,271 @@
+function JSONEditorBase() {
+  this.history = [];
+  this.historyPointer = -1;
+  this.builderShowing = true;
+}
+
+function JSONEditor(wrapped, width, height) {
+  this.wrapped = $(wrapped);
+
+  this.wrapped.wrap('<div class="container"></div>');
+  this.container = $(this.wrapped.parent());
+  this.container.css("position", "relative");
+          
+  this.container.width(width).height(height);
+  this.wrapped.width(width).height(height);
+  this.container.css("position", "relative");
+  
+  this.rebuild();
+  
+  return this;
+}
+JSONEditor.prototype = new JSONEditorBase();
+
+JSONEditor.prototype.BRACE = function(key, struct) {
+  var self = this;
+  return $('<a class="icon" href="#"><strong>{</strong></a>').click(function(e) {
+    struct[key] = { "??": struct[key] };
+    self.rebuild();
+    return false;
+  });
+};
+
+JSONEditor.prototype.BRACKET = function(key, struct) {
+  var self = this;
+  return $('<a class="icon" href="#"><strong>[</a>').click(function(e) {
+    struct[key] = [ struct[key] ];
+    self.rebuild();
+    return false;
+  });
+};
+
+JSONEditor.prototype.DELETE = function(key, struct, layerOnly) {
+  var self = this;
+  return $('<a class="icon" href="#"><img src="/images/icons/delete.png" border=0/></a>').click(function(e) {
+    var didSomething = false;
+    if (struct[key] instanceof Array) {
+      if(struct[key].length > 0) {
+        struct[key] = struct[key][0];
+        didSomething = true;
+      }
+    } else if (struct[key] instanceof Object) {
+      for (var i in struct[key]) {
+        struct[key] = struct[key][i];
+        didSomething = true;
+        break;
+      }
+    }
+    if (didSomething) {
+      self.rebuild();
+      return false;
+    }
+    if (struct instanceof Array) {
+      struct.splice(key, 1);
+    } else {
+      delete struct[key];
+    }
+    self.rebuild();
+    return false;
+  });
+};
+
+JSONEditor.prototype.ADD = function(struct) {
+  var self = this;
+  return $('<a class="icon" href="#"><img src="/images/icons/add.png" border=0/></a>').click(function(e) {
+    if (struct instanceof Array) {
+      struct.push('??');
+    } else {
+      struct['??'] = '??';
+    }
+    self.rebuild();
+    return false;
+  });
+};
+
+JSONEditor.prototype.undo = function() {
+  this.saveStateIfTextChanged();
+  if (this.historyPointer > 0) this.historyPointer -= 1;
+  this.restore();
+};
+      
+JSONEditor.prototype.redo = function() {
+  if (this.historyPointer + 1 < this.history.length) {
+    this.saveStateIfTextChanged();
+    this.historyPointer += 1;
+    this.restore();
+  }
+};
+
+JSONEditor.prototype.showBuilder = function() {
+    this.setJsonFromText();
+    this.rebuild();
+    this.builder.show();
+    this.wrapped.hide();  
+};
+
+JSONEditor.prototype.showText = function() {
+    this.builder.hide();
+    this.wrapped.show();
+};
+
+JSONEditor.prototype.toggleBuilder = function() {
+    if(this.builderShowing){
+      this.showText();
+    } else {
+      this.showBuilder();
+    }
+    this.builderShowing = !this.builderShowing;
+};
+
+JSONEditor.prototype.saveStateIfTextChanged = function() {
+  if (JSON.stringify(this.json, null, 2) != this.wrapped.get(0).value) {
+    this.saveState(true);
+  }
+};
+
+JSONEditor.prototype.restore = function() {
+  if (this.history[this.historyPointer]) {
+    this.wrapped.get(0).value = this.history[this.historyPointer];
+    this.rebuild(true);
+  }
+};
+
+JSONEditor.prototype.saveState = function(skipStoreText) {
+  if (this.json) {
+    if (!skipStoreText) this.storeToText();
+    var text = this.wrapped.get(0).value;
+    if (this.history[this.historyPointer] != text) {
+      this.historyTruncate();
+      this.history.push(text);
+      this.historyPointer += 1;
+    }
+  }
+};
+
+JSONEditor.prototype.historyTruncate = function() {
+  if (this.historyPointer + 1 < this.history.length) {
+    this.history.splice(this.historyPointer + 1, this.history.length - this.historyPointer);
+  }
+};
+
+JSONEditor.prototype.storeToText = function() {
+  this.wrapped.get(0).value = JSON.stringify(this.json, null, 2);
+};
+
+JSONEditor.prototype.rebuild = function(doNotRefreshText) {
+  if (!this.json) this.setJsonFromText();
+  if (this.json && !doNotRefreshText) {
+    this.saveState();
+  }
+  this.cleanBuilder();
+  this.setJsonFromText();
+  this.alreadyFocused = false;
+  this.build(this.json, this.builder);
+};
+
+JSONEditor.prototype.setJsonFromText = function() {
+  this.json = JSON.parse(this.wrapped.get(0).value);
+};
+
+JSONEditor.prototype.logJSON = function() {
+  console.log(JSON.stringify(this.json, null, 2));
+};
+
+JSONEditor.prototype.cleanBuilder = function() {
+  if (!this.builder) {
+    this.builder = $('<div class="builder"></div>');
+    this.container.append(this.builder);
+  } else {
+    this.builder.text('');
+  }
+  
+  this.builder.css("position", "absolute").css("top", 0).css("left", 0);
+  this.builder.width(this.wrapped.width()).height(this.wrapped.height());
+  this.wrapped.css("position", "absolute").css("top", 0).css("left", 0);        
+};
+
+JSONEditor.prototype.edit = function(e, key, struct, kind){
+  var self = this;
+  var form = $("<form></form>").css('display', 'inline');
+  var input = document.createElement("INPUT");
+  input.value = e.text();
+  var onblur = function() {
+    var val = input.value;
+    if(kind == 'key') {
+      struct[val] = struct[key];
+      if (key != val) delete struct[key];
+    } else {
+      struct[key] = val;
+    }
+    e.text(val);
+    e.get(0).editing = false;
+    if (key != val) self.rebuild();
+    return false;
+  };
+  $(input).blur(onblur);
+  $(form).submit(onblur).append(input);
+  $(e).html(form);
+  input.focus();
+};
+
+JSONEditor.prototype.editable = function(text, key, struct, kind) {
+  var self = this;
+  var elem = $('<span class="editable" href="#"></span>').text(text).click(function(e) {
+    if (!this.editing) {
+      this.editing = true;
+      self.edit($(this), key, struct, kind);
+    }
+    return true;
+  });
+  
+  // Auto-edit '??' keys and values.
+  if (text == '??' && !this.alreadyFocused) {
+    this.alreadyFocused = true;
+    elem.click();
+    $(this).oneTime(100, function() { // Because JavaScript is annoying and we need to focus once the current stuff is done.
+      elem.find('input').focus().select();
+    });
+  }
+  return elem;
+}
+
+JSONEditor.prototype.build = function(json, node, parent, key) {
+  if(json instanceof Array){
+    var bq = $(document.createElement("BLOCKQUOTE"));
+    bq.append($("<div>[</div>"));
+    
+    if (parent) bq.prepend(this.DELETE(key, parent));
+    bq.prepend(this.ADD(json));
+
+    for(var i = 0; i < json.length; i++) {
+      var innerbq = $(document.createElement("BLOCKQUOTE"));
+      this.build(json[i], innerbq, json, i);
+      bq.append(innerbq);
+    }
+    
+    bq.append($("<div>]</div>"));
+    node.append(bq);
+  } else if (json instanceof Object) {
+    var bq = $(document.createElement("BLOCKQUOTE"));
+    bq.append($('<div>{</div>'));
+
+    if (parent) bq.prepend(this.DELETE(key, parent, true));
+    bq.prepend(this.ADD(json));
+
+    for(var i in json){
+      var innerbq = $(document.createElement("BLOCKQUOTE"));
+      innerbq.append(this.editable(i.toString(), i.toString(), json, 'key').wrap('<b class="key"></b>').parent());
+      innerbq.append(document.createTextNode(': ')); 
+      this.build(json[i], innerbq, json, i);
+      bq.append(innerbq);
+    }
+    
+    bq.append($('<div>}</div>'));
+    node.append(bq);
+  } else {
+    node.append(this.editable(json.toString(), key, parent, 'value').wrap('<span class="val"></span>').parent());
+    if (parent) node.prepend(this.DELETE(key, parent));
+    node.prepend(this.BRACE(key, parent));
+    node.prepend(this.BRACKET(key, parent));
+  }
+};
